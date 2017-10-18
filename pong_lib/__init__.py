bf35d424a63acd037b85aed1733566682d131415
r.rodenburg <r.rodenburg@anchormen.nl>
2017-10-18 13:15:01 +0200

    dataset generation and game loop are written in the pong_algoritm

--- pong_lib/__init__.py
+++ pong_lib/__init__.py
@@ -19,7 +19,8 @@ class Pong(object):
                 pad_heigth = 80, 
                 pad_velocity = 8, 
                 pad_velocity_ai = 1,
-                DistPadWall = 20
+                DistPadWall = 20,
+                ball_velocity = 1
                 ):
                 
         self.number_of_players = number_of_players
@@ -31,6 +32,7 @@ class Pong(object):
         self.pad_velocity = pad_velocity
         self.pad_velocity_ai = pad_velocity_ai
         self.DistPadWall = DistPadWall
+        self.ball_velocity = ball_velocity
         
         #canvas declaration
         self.window = pygame.display.set_mode((self.width , self.heigth))
@@ -40,10 +42,21 @@ class Pong(object):
         self.padL_vel = 0 #y-velocity of left pad
         self.padR_vel = 0 #y-velocity of right pad
         self.padL_pos= np.array([DistPadWall, self.heigth/ 2 - self.pad_heigth / 2]) #these positions denote the upperleft corner of the pad; (0,0) is UL corner of screen
-        self.padR_pos= np.array([self.width - DistPadWall - self.pad_width, self.heigth / 2 - self.pad_heigth / 2])
+        self.padR_pos= np.array([self.width - self.DistPadWall - self.pad_width, self.heigth / 2 - self.pad_heigth / 2])
+        
+    def initialize_ball_pad_positions(self):
+        #initialize ball position and velocity
+        
+        self.velinit_list = list(range(3,5)) + list(range(-4,-2))
+        self.ball_pos = np.array([self.width / 2 - self.ball_radius, self.heigth / 2 - self.ball_radius])
+        self.ball_vel = self.ball_velocity* np.array([random.choice(self.velinit_list),random.choice(self.velinit_list)])
+        
+        self.padL_pos= np.array([self.DistPadWall, self.heigth/ 2 - self.pad_heigth / 2]) #these positions denote the upperleft corner of the pad; (0,0) is UL corner of screen
+        self.padR_pos= np.array([self.width - self.DistPadWall - self.pad_width, self.heigth / 2 - self.pad_heigth / 2])
         
     #function to draw the current screen:
     def draw(self, canvas, _padL_pos, _padR_pos, _ball_pos):
+        self.window.fill(Pong.black)
         pygame.draw.rect(canvas, Pong.white, (_padL_pos[0],_padL_pos[1], self.pad_width, self.pad_heigth)) #draws L pad
         pygame.draw.rect(canvas, Pong.white, (_padR_pos[0],_padR_pos[1], self.pad_width, self.pad_heigth)) #draws R pad
         pygame.draw.rect(canvas, Pong.white, (_ball_pos[0],_ball_pos[1], 2*self.ball_radius, 2*self.ball_radius)) #draws ball
@@ -78,9 +91,9 @@ class Pong(object):
     def updatepad_pos(self, velocity, currentypos): #returns new y-position of a pad
         if currentypos > 0 and currentypos < (self.heigth - self.pad_heigth):
             return currentypos +  velocity
-        elif currentypos == 0 and velocity > 0:
+        elif currentypos <= 0 and velocity > 0:
             return currentypos +  velocity
-        elif currentypos == (self.heigth - self.pad_heigth) and velocity < 0:
+        elif currentypos >= (self.heigth - self.pad_heigth) and velocity < 0:
             return currentypos +  velocity
         else:
             return currentypos
@@ -104,18 +117,22 @@ class Pong(object):
     def wincheck(self, ball_position, ball_velocity, score): #Check if player wins
         if ball_position[0] <= 0:
             score[0] += 1
+            reward = -1
             print("player right wins, current score is {}".format(score))
             (ball_position, ball_velocity) = self.initialize_ball()
         if (ball_position[0] + 2 * self.ball_radius) >= self.width:
             score[1] += 1
+            reward = 1
             print('player left wins, current score is {}'.format(score))
             (ball_position, ball_velocity) = self.initialize_ball()
-        return (score, ball_position, ball_velocity)
+        else:
+            reward = 0
+        return (score, ball_position, ball_velocity, reward)
         
     def initialize_ball(self):
         velinit_list = list(range(3,5)) + list(range(-4,-2))
         ball_pos = np.array([self.width / 2 - self.ball_radius, self.heigth / 2 - self.ball_radius])
-        ball_vel = np.array([random.choice(velinit_list),random.choice(velinit_list)])
+        ball_vel = self.ball_velocity * np.array([random.choice(velinit_list),random.choice(velinit_list)])
         return (ball_pos, ball_vel)
         
     def updatepad_pos_ai(self, velocity, currentypos, ball_position):
@@ -131,7 +148,6 @@ class Pong(object):
         pygame.display.set_caption('PONG')
         self.fps = pygame.time.Clock()
         self.running = True
-        (self.ball_pos, self.ball_vel) = self.initialize_ball()
         while self.running:
             (self.ball_vel, self.ball_pos) = self.updateball_pos(self.ball_vel, self.ball_pos, self.padL_pos, self.padR_pos)
             self.padL_pos[1] = self.updatepad_pos(self.padL_vel,self.padL_pos[1])
@@ -139,7 +155,6 @@ class Pong(object):
                 self.padR_pos[1] = self.updatepad_pos(self.padR_vel,self.padR_pos[1])
             else:
                 self.padR_pos[1] = self.updatepad_pos_ai(self.pad_velocity_ai, self.padR_pos[1], self.ball_pos)
-            self.window.fill(Pong.black)
             self.draw(self.window, self.padL_pos, self.padR_pos, self.ball_pos)
             (self.score, self.ball_pos, self.ball_vel) = self.wincheck(self.ball_pos, self.ball_vel, self.score)
             for event in pygame.event.get():
@@ -153,4 +168,80 @@ class Pong(object):
                     (self.padL_vel,self.padR_vel) = self.keyup(event, self.padL_vel, self.padR_vel)
             pygame.display.update()
             self.fps.tick(frames_per_sec)
+    
+    def game_init(self):
+        pygame.display.set_caption('PONG')
+        self.fps = pygame.time.Clock()
+        
+    def game_loop(self, frames_per_sec = 60):
+        (self.ball_vel, self.ball_pos) = self.updateball_pos(self.ball_vel, self.ball_pos, self.padL_pos, self.padR_pos)
+        self.padL_pos[1] = self.updatepad_pos(self.padL_vel,self.padL_pos[1])
+        if self.number_of_players == 2:
+            self.padR_pos[1] = self.updatepad_pos(self.padR_vel,self.padR_pos[1])
+        else:
+            self.padR_pos[1] = self.updatepad_pos_ai(self.pad_velocity_ai, self.padR_pos[1], self.ball_pos)
+        
+        self.draw(self.window, self.padL_pos, self.padR_pos, self.ball_pos)
+        (self.score, self.ball_pos, self.ball_vel, reward) = self.wincheck(self.ball_pos, self.ball_vel, self.score)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+                self.running = False
+            if event.type == KEYDOWN: #keydown means you are pushing any button
+                (self.padL_vel, self.padR_vel) = self.keydown(event, self.padL_vel, self.padR_vel)
+            if event.type == KEYUP:
+                (self.padL_vel,self.padR_vel) = self.keyup(event, self.padL_vel, self.padR_vel)
+        pygame.display.update()
+        self.fps.tick(frames_per_sec)
+        return reward
+        
+    def updatepad_pos_action(self, velocity, currentypos, action): #returns new y-position of a pad
+        if currentypos < (self.heigth - self.pad_heigth) and action == 'down':
+            return currentypos + velocity
+        if currentypos > 0 and action == 'up':
+            return currentypos - velocity
+        if currentypos > 0 and currentypos < (self.heigth - self.pad_heigth) and action == 'stay':
+            return currentypos
+        else: 
+            return currentypos
+    
+    def reward(self, ball_position, ball_velocity): #Check if player wins
+        if ball_position[0] <= 0:
+            reward = -1
+        elif (ball_position[0] + 2 * self.ball_radius) >= self.width:
+            reward = 1
+        else:
+            reward = 0
+        return reward
+        
+    def game_loop_action_input(self, action, frames_per_sec = 60):
+        self.padL_vel = 8
+        (self.ball_vel, self.ball_pos) = self.updateball_pos(self.ball_vel, self.ball_pos, self.padL_pos, self.padR_pos)
+            
+        # Observe if a game has been won or lost
+        reward = self.reward(self.ball_pos, self.ball_vel)
+            
+        # Update position of the left pad according to the action input    
+        self.padL_pos[1] = self.updatepad_pos_action(self.padL_vel, self.padL_pos[1], action)
+        # Let ai update the position of the right pad
+        self.padR_pos[1] = self.updatepad_pos_ai(self.pad_velocity_ai, self.padR_pos[1], self.ball_pos)
+        
+        # Draw the screen
+        self.draw(self.window, self.padL_pos, self.padR_pos, self.ball_pos)
+        
+        #if reward == -1 or reward == 1:
+        #    running = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+                self.running = False
+        #s    if event.type == KEYDOWN: #keydown means you are pushing any button
+        #s        (self.padL_vel, self.padR_vel) = self.keydown(event, self.padL_vel, self.padR_vel)
+        #s    if event.type == KEYUP:
+        #s        (self.padL_vel,self.padR_vel) = self.keyup(event, self.padL_vel, self.padR_vel)
+        pygame.display.update()
+        self.fps.tick(frames_per_sec)
+        return reward
             
