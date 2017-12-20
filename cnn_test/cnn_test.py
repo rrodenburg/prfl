b8dc1bca6a1d5b81badbdaae6538a88c66dccae7
r.rodenburg <r.rodenburg@anchormen.nl>
2017-12-20 12:43:18 +0100

    cnn with xavier initialization

--- cnn_test/cnn_test.py
+++ cnn_test/cnn_test.py
@@ -3,176 +3,133 @@ import tensorflow as tf
 import random
 
 memory = np.load('../../data/repl_mem.npy')
+#memory = memory[:32]
 
+print(len(memory))
 print(np.take(memory, indices = 0, axis = 1)[0].shape)
 
 ### Define neural network ### https://www.tensorflow.org/tutorials/layers
 
-learning_rate = 0.001
+learning_rate = 0.0001
 frame_stacks = 4
 width = 84
 heigth = 84
+mini_batch_size = 32
 
 tf.reset_default_graph()
-#def convnet(mode):
-#    x = tf.placeholder("int32",(-1,width,heigth,frame_stacks)) #input states
-#    y = tf.placeholder("float32",(-1)) #y-values for loss function, as described in atari paper
-#    a = tf.placeholder("int32",(-1)) #actions played in batch; 0: nothing, 1: up, 2: down
-#
-#    # Convolutional Layer #1
-#    conv1 = tf.layers.conv2d(
-#      inputs = x,
-#      filters = 5,
-#      kernel_size = [8, 8],
-#      strides = (4,4),
-#      padding = "valid", #valid means no padding
-#      activation = tf.nn.relu)
-#
-#    # Output layer (dense layer)
-#    conv1_flat = tf.reshape(conv1,[-1,20*20*5])
-#    output = tf.layers.dense(inputs=conv1_flat, units = 3)
-#    best_action = tf.argmax(input=output, axis=1)
-
-#    predictions = {
-#      # Generate predictions (for PREDICT and EVAL mode)
-#        "best_action": tf.argmax(input=output, axis=1), 
-#        "max_Q_value": tf.reduce_max(input=output,axis=1, name = 'Q_max')
-#      }
-#
-#    if mode == tf.estimator.ModeKeys.PREDICT:
-#        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-#
-#    # loss function
-#    onehot_actions = tf.one_hot(indices=a, depth=3)#batch-actions converted, s.t. 0 = nothing --> (1,0,0) etc
-#    onehot_actions = tf.transpose(onehot_actions)
-#    Q_values = tf.diag_part(tf.matmul(output,onehot_actions))
-#    loss = tf.reduce_mean(tf.square(y - Q_values),axis=0)
-#
-#  # Configure the Training Op (for TRAIN mode)
-#    if mode == tf.estimator.ModeKeys.TRAIN:
-#        optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate,momentum = 0.95,epsilon=0.01)
-#        train_op = optimizer.minimize(
-#            loss=loss,
-#            global_step=tf.train.get_global_step())
-#        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-
-#def main():
-#	train_data = np.take(memory, indices = 0, axis = 1)
-#
-#	# Create the Estimator
-#	mnist_classifier = tf.estimator.Estimator(
-#    	model_fn=convnet, model_dir="/tmp/convnet_model")
-#
-#	# Set up logging for predictions
-#  	tensors_to_log = {"max_Q_value": "Q_max"}
-#  	logging_hook = tf.train.LoggingTensorHook(
-#      tensors=tensors_to_log, every_n_iter=50)
-
-
-# Generate predictions https://www.tensorflow.org/get_started/estimator
-'''
-predict_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"x": new_samples},
-    num_epochs=1,
-    shuffle=False)
-
-predictions = list(classifier.predict(input_fn=predict_input_fn))
-predicted_classes = [p["classes"] for p in predictions]
-'''
 
 x = tf.placeholder("float32",(None,width,heigth,frame_stacks)) #input states
 y = tf.placeholder("float32",(None)) #y-values for loss function, as described in atari paper
 a = tf.placeholder("int32",(None)) #actions played in batch; 0: nothing, 1: up, 2: down
-# Convolutional Layer #1
+
+# Convolutional Layers
 conv1 = tf.layers.conv2d(
   inputs = x,
   filters = 32,
   kernel_size = [8, 8],
   strides = (4,4),
   padding = "valid", #valid means no padding
+  kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(),
+  bias_initializer=tf.zeros_initializer(),
   activation = tf.nn.relu) #output is 20x20x32
 
+conv2 = tf.layers.conv2d(
+  inputs = conv1,
+  filters = 64,
+  kernel_size = [4, 4],
+  strides = (2,2),
+  padding = "valid", #valid means no padding
+  kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(),
+  bias_initializer=tf.zeros_initializer(),
+  activation = tf.nn.relu) #output is 9x9x64
+
+conv3 = tf.layers.conv2d(
+  inputs = conv2,
+  filters = 64,
+  kernel_size = [3, 3],
+  strides = (1,1),
+  padding = "valid", #valid means no padding
+  kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(),
+  bias_initializer=tf.zeros_initializer(),
+  activation = tf.nn.relu) #output is 7x7x64
+
 # Output layer (dense layer)
-conv1_flat = tf.reshape(conv1,[-1,20*20*32])
-output = tf.layers.dense(inputs=conv1_flat, units = 3)
-best_action = tf.argmax(input=output, axis=1)
-max_Q_value = tf.reduce_max(output,axis=1, name = 'Q_max')
+conv3_flat = tf.reshape(conv3,[-1,7*7*64])
+FC = tf.layers.dense(inputs=conv3_flat, units = 512, kernel_initializer = tf.contrib.layers.xavier_initializer(), activation = tf.nn.relu, bias_initializer=tf.zeros_initializer())
+output = tf.layers.dense(inputs = FC, units = 3,  kernel_initializer = tf.contrib.layers.xavier_initializer(), bias_initializer=tf.zeros_initializer())
+best_action = tf.argmax(input = output, axis = 1)
+max_Q_value = tf.reduce_max(output, axis = 1, name = 'Q_max')
 
 #loss function
-onehot_actions = tf.one_hot(indices=a, depth=3)#batch-actions converted, s.t. 0 = nothing --> (1,0,0) etc
+onehot_actions = tf.one_hot(indices=a, depth=3) #batch-actions converted, s.t. 0 = nothing --> (1,0,0) etc
 onehot_actions = tf.transpose(onehot_actions)
 Q_values = tf.diag_part(tf.matmul(output,onehot_actions))
-loss = tf.reduce_mean(tf.square(y - Q_values),axis=0)
+loss = tf.reduce_mean(tf.square(y - Q_values), axis = 0)
 
-optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate,momentum = 0.95,epsilon=0.01)
+optimizer = tf.train.RMSPropOptimizer(learning_rate = learning_rate, momentum = 0.95, epsilon = 0.01)
 
 global_step = tf.Variable(0, name='global_step', trainable=False)
 train_op = optimizer.minimize(
             loss=loss,
             global_step=global_step)
 
-#model_params = {
-#	'learning_rate' : learning_rate
-#				}
-
-#Q_estimator = tf.estimator.Estimator(
-#    	model_fn=convnet, params=model_params)#, model_dir="/tmp/convnet_model")
-
-predict_x = np.take(memory, indices = 3, axis = 1)[:32]
-predict_x = [x.astype('float32') for x in predict_x]
-#print(predict_x.shape)
-
-print(type(memory))
-
-def mini_batch_sample(memory):
+def mini_batch_sample(memory, mini_bach_size):
 #Select random mini-batch of 32 transitions
-	idx = np.random.randint(len(memory), size = 32)
+	idx = np.random.randint(len(memory), size = mini_bach_size)
 	mini_batch = memory[idx]
 
 	mini_batch_x = [x[0].astype('float32') for x in mini_batch]
 	mini_batch_y = [x[3].astype('float32') for x in mini_batch]
-
-	#trans_dict = {
-	#		  'stay' : 0,
-	#		  'up' : 1,
-	#		  'down': 0
-	#		  }
-
 	reward = [x[2] for x in mini_batch]
 
 	mini_batch_action = [x[1] for x in mini_batch]
 	return mini_batch_x, mini_batch_y, mini_batch_action, reward
 
 def create_y(reward, q_values):
-	return [q_value[idx] if x is 0 else reward[idx] for idx,x in enumerate(reward)]
+	return [q_values[idx] if x is 0 else reward[idx] for idx,x in enumerate(reward)]
 
+def determine_action(frame_stack):
+	frame_stack = [[x.astype('float32') for x in predict_x]]
+	action = sess.run(best_action, feed_dict = {x : frame_stack})
+	return action[0]
+
+def backprop(memory, mini_batch_size):
+	mini_batch_x, mini_batch_y, mini_batch_action, reward = mini_batch_sample(memory, mini_batch_size)
+	_, q_value = sess.run([best_action, max_Q_value], {x: mini_batch_y})
+
+	target_y = create_y(reward, q_value) # select reward as y if episode had ended
+
+	feed_dict_train = {
+					x : mini_batch_x,
+					y : target_y,
+					a : mini_batch_action
+					}
+
+	_, loss_value = sess.run([train_op, loss], feed_dict = feed_dict_train)
+
+	return loss_value
 
 
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 
-mini_batch_x, mini_batch_y, mini_batch_action, reward = mini_batch_sample(memory)
+mini_batch_x, mini_batch_y, mini_batch_action, reward = mini_batch_sample(memory, mini_batch_size)
 _, q_value = sess.run([best_action, max_Q_value], {x: mini_batch_y})
 
 target_y = create_y(reward, q_value)
 
-for i in range(50):
-	
-	#print(q_value)
-	feed_dict_train = {
-					x : mini_batch_x,
-					y : target_y,
-					a : mini_batch_action
-					}
-	
-	#lossy = sess.run([loss], feed_dict = feed_dict_train)
-	_, loss_value = sess.run([train_op, loss], feed_dict = feed_dict_train)
-	print(loss_value)
+loss_value = 0
 
-#variables = sess.run(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES))
-variables = tf.trainable_variables()
+for i in range(1000):
+	
+	#action = determine_action(predict_x)
+
+	loss_value += backprop(memory, mini_batch_size)
+	
+	if i % 50 == 0: 
+		print('loss after ', i, ' backprop cycles = ', loss_value)
+		loss_value = 0
 
 
 sess.close()
