0fd7e1c5dbe8ca1c80e5bd32c4261d51591f1761
r.rodenburg <r.rodenburg@anchormen.nl>
2018-01-02 09:22:54 +0100

    cleaned up code

--- pong_algorithm_cnn_cleancode.py
+++ pong_algorithm_cnn_cleancode.py
@@ -20,18 +20,18 @@ game = pong_lib.Pong(
                 pad_width = 4, 
                 pad_heigth = 14, 
                 pad_velocity = 8, 
-                pad_velocity_ai = 4,
+                pad_velocity_ai = 2,
                 DistPadWall = 4,
-                ball_velocity = 1.5)
+                ball_velocity = 0.7)
 game.game_init()
 
 ### Hyperparameter settings
 epsilon = 0.9 #probability to play a random action
-frame_stacks = 2
+frame_stacks = 4
 games_to_play = 10000
 max_length_dataset = 1e6
 learning_rate = 0.00025
-replay_start_size = 1000
+replay_start_size = 5000
 mini_batch_size = 16
 
 #tensorflow function
@@ -107,8 +107,7 @@ def mini_batch_sample(memory, mini_batch_size):
 def create_y(reward, q_values):
 	return [q_values[idx] if x is 0 else reward[idx] for idx,x in enumerate(reward)]
 
-def determine_action(frame_stack):
-	#frame_stack = np.transpose(frame_stack, (1, 0, 2))
+def pick_greedy_action(frame_stack):
 	frame_stack = [[x.astype('float32') for x in frame_stack]]
 	action, q_value = sess.run([best_action, max_Q_value], feed_dict = {x : frame_stack})
 	return action[0], q_value[0]
@@ -129,12 +128,88 @@ def backprop(memory, mini_batch_size):
 
 	return loss_value
 
+def add_frame(state_list, frame_count):
+	#observe state
+	frame = pygame.surfarray.pixels2d(game.window).astype('float')
+	frame *= 1/np.amax(frame) # Normalize the pixel intensities
+	state_list[frame_count] = frame.astype('int8') #save as int8 to reduce memory usage
+	return state_list
+
+def determine_action(state, epsilon, episode_state_count, total_transition_count, replay_start_size):
+	if total_transition_count > replay_start_size and epsilon > random.uniform(0, 1) and episode_state_count > 0:
+		action, q_value = pick_greedy_action(state) ## Let model pick next action to play, 0 = stay, 1 = up, 2 = down
+	else:
+	    action = np.random.randint(3) ## Play random action
+	    q_value = None
+	return action, q_value
+
+def rolling_avg(average, new_data_point, alpha):
+	average = (1 - alpha) * average + alpha * new_data_point
+	return average
+
+def state_accumulate(action, frame_stacks, episode_running):
+	frame_accumulation = True
+	frame_count = 0
+
+	next_state = np.empty([frame_stacks,84,84], dtype = 'int8') #initialize array to save frames
+	
+	reward = 0 # initialize reward
+	while frame_accumulation == True:
+	    
+	    #observe reward
+	    if reward == 0: # does not allow to obtain a reward of +2 or -2 if game termination does not occur instantly
+	        reward += game.game_loop_action_input(action)
+	        
+	    if reward != 0:
+	        frame_accumulation = False
+	        episode_running = False #if game ends, start a new game
+	        next_state = np.empty([84,84,frame_stacks], dtype = 'int8') # Return empty array of correct shape such that tensorflow can process it
+	        return next_state, reward, episode_running
+	    
+	    #observe state
+	    next_state = add_frame(next_state, frame_count)
+	    
+	    if frame_count == (frame_stacks - 1):
+	        frame_count = 0
+	        frame_accumulation = False
+	    
+	    frame_count += 1
+
+	next_state = np.transpose(next_state,(1,2,0)) #transpose matrix to get it in suitable shape for tensorflow
+	return next_state, reward, episode_running
+
+def repl_memory_append(dataset, state, action, reward, next_state, total_transition_count, max_length_dataset):
+	dataset.append((state, action, reward, next_state)) 
+	total_transition_count += 1
+	if total_transition_count > max_length_dataset:
+	    dataset.pop(0)
+	return dataset, total_transition_count
+
+def update_nn(dataset, mini_batch_size, loss_value, backprop_cycles, total_transition_count, replay_start_size):
+	if total_transition_count > replay_start_size:
+		loss_value += backprop(dataset, mini_batch_size)
+		backprop_cycles += 1
+	return loss_value, backprop_cycles
+
+def display_stats(backprop_cycles, n_games_played, games_won, loss_value, running_score_mean, q_value_mean):
+	print('backprop_cycles : {}, episodes : {} , games won : {} , loss : {} , running score avg : {} , running q_value mean : {}'.format(backprop_cycles, n_games_played, games_won, 
+		loss_value, running_score_mean, q_value_mean))
+	loss_value = 0
+	return loss_value
+
+def accumulate_stats(reward, n_games_played, games_won, running_score_mean, running_score_alpha, q_value_mean, q_value, q_value_alpha):
+	if reward != 0:
+		n_games_played += 1
+		running_score_mean = rolling_avg(running_score_mean, reward, running_score_alpha)
+	if reward == 1:
+		games_won += 1
+	if q_value is not None:
+		q_value_mean = rolling_avg(q_value_mean, q_value, q_value_alpha)
+	return n_games_played, games_won, running_score_mean, q_value_mean
 
 
 ### initialize dataset
 dataset = []
-
-
 total_transition_count = 0
 
 n_games_played = 0
@@ -142,11 +217,10 @@ games_won = 0
 
 # stats initialization
 running_score_mean = 0.0
-running_score_alpha = 0.01
-running_score_mean_list = []
+running_score_alpha = 0.05
 
 q_value_mean = 0.0
-q_value_alpha = 0.01
+q_value_alpha = 0.05
 
 loss_value = 0.0
 backprop_cycles = 0 
@@ -157,95 +231,47 @@ init = tf.global_variables_initializer()
 sess.run(init)
 
 running = True
-while running: #runs once through the loop per episode
-    #play as long as game lasts
+while running: 
+    
     episode_running = True
-    #print('a new pong game has started!')
+
     game.initialize_ball_pad_positions()
-    
-    #initialize a current state, the transition from the initialed state will later be deleted from the dataset
-    
-    state = np.zeros([frame_stacks,84,84], dtype = 'int8')
     
     episode_state_count = 0 #counts the state number of the current episode
     
-    while episode_running == True:
-        
-        ## Pick action following greedy policy; 1 action per episode
-        if total_transition_count > replay_start_size and epsilon > random.uniform(0, 1) and episode_state_count > 0:
-	        	action, q_value = determine_action(state)## Let model pick next action to play, 0 = stay, 1 = up, 2 = down
-	        	#print('network action :' , action)
-	        	q_value_mean = (1 - q_value_alpha) * q_value_mean + q_value_alpha * q_value
-        else:
-            action = np.random.randint(3) ## Play random action
-    
-        #initialize array to save frames
-        next_state = np.empty([frame_stacks,84,84], dtype = 'int8')
-        
-        # Run action and observe reward and next state
-        frame_accumulation = True
-        frame_count = 0
-        
-        reward = 0 # initialize reward
-        while frame_accumulation == True:
-            
-            #observe reward
-            if reward == 0: # does not allow to obtain a reward of +2 or -2 if game termination does not occur instantly
-                reward += game.game_loop_action_input(action)
-                
-            if reward != 0:
-                #print('someone won! game must terminate!')
-                n_games_played += 1
-                if reward == 1:
-                	games_won += 1
-                running_score_mean = (1 - running_score_alpha) * running_score_mean + running_score_alpha * reward #Checck if correct!
-                running_score_mean_list.append(running_score_mean)
-                frame_accumulation = False
-                episode_running = False #if game ends, start a new game
-            
-            #observe state
-            frame = pygame.surfarray.pixels2d(game.window).astype('float')
-            frame *= 1/np.amax(frame) # Normalize the pixel intensities
-            next_state[frame_count] = frame.astype('int8') #save as int8 to reduce memory usage
-            
-            if frame_count == (frame_stacks - 1):
-                frame_count = 0
-                frame_accumulation = False
-            
-            frame_count += 1
+    while episode_running == True: #runs once through the loop per episode
 
-        next_state = np.transpose(next_state,(1,2,0)) #transpose matrix to get it in suitable shape for tensorflow
+        if episode_state_count == 0: # play no action during first frames to initialize
+        	action = 0 
+
+        	next_state, reward, episode_running = state_accumulate(action, frame_stacks, episode_running)
         
         #write an entry into the dataset, do backprop and print stats
-        if episode_state_count > 0:
-        	dataset.append((state, action, reward, next_state)) 
-        	total_transition_count += 1
-        	if total_transition_count > max_length_dataset:
-        	    dataset.pop(0)
+        else:
+			## Pick action following greedy policy; 1 action per episode
+        	action, q_value = determine_action(state, epsilon, episode_state_count, total_transition_count, replay_start_size)
+        	
+        	next_state, reward, episode_running = state_accumulate(action, frame_stacks, episode_running)
+	
+    		# Accumulate statistics
+        	n_games_played, games_won, running_score_mean, q_value_mean = accumulate_stats(reward, n_games_played, games_won, running_score_mean, running_score_alpha, q_value_mean, q_value, q_value_alpha)
 
-        	# perform backprop
-        	if total_transition_count > replay_start_size:
-        		loss_value += backprop(dataset, mini_batch_size)
-        		backprop_cycles += 1
-        		#print('backprop	done')
+        	dataset, total_transition_count = repl_memory_append(dataset, state, action, reward, next_state, total_transition_count, max_length_dataset)
+
+        	loss_value, backprop_cycles = update_nn(dataset, mini_batch_size, loss_value, backprop_cycles, total_transition_count, replay_start_size)
 
         	# display statistics
         	if total_transition_count % 200 == 0:
-        		print('backprop_cycles : {}, episodes : {} , games won : {} , loss : {} , running score avg : {} , running q_value mean : {}'.format(backprop_cycles, n_games_played, games_won, 
-        			loss_value, running_score_mean, q_value_mean))
-        		loss_value = 0
+        		loss_value = display_stats(backprop_cycles, n_games_played, games_won, loss_value, running_score_mean, q_value_mean)
 
         episode_state_count += 1
-        #print(len(dataset))
         
         #Set current_state to the previous state
         state = next_state
     
     if n_games_played == games_to_play:
         pygame.quit()
-        running = False
-            
-#np.save('../data/repl_mem', dataset)                   
+        running = False                 
 
 sys.exit()
                 
